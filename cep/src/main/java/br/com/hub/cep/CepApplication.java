57ec6b16d0c8dfe39a184996af02f100fea74944
patriciasouzas <patriciasouzaa89@gmail.com>
2023-03-13 22:27:57 -0300

    added modifications

--- cep/src/main/java/br/com/hub/cep/CepApplication.java
+++ cep/src/main/java/br/com/hub/cep/CepApplication.java
@@ -2,12 +2,19 @@ package br.com.hub.cep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CepApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CepApplication.class, args);
-	}
+    private static ConfigurableApplicationContext ctx;
+
+    public static void main(String[] args) {
+        ctx = SpringApplication.run(CepApplication.class, args);
+    }
+
+    public static void close(int code) {
+        SpringApplication.exit(ctx, () -> code);
+    }
 
 }
