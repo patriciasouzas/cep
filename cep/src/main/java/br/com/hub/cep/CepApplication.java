51a6538180540efc95588540362d47099882d17c
patriciasouzas <patriciasouzaa89@gmail.com>
2023-03-14 21:01:38 -0300

    adjustments

--- cep/src/main/java/br/com/hub/cep/CepApplication.java
+++ cep/src/main/java/br/com/hub/cep/CepApplication.java
@@ -3,8 +3,12 @@ package br.com.hub.cep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableJpaRepositories
 public class CepApplication {
 
     private static ConfigurableApplicationContext ctx;
